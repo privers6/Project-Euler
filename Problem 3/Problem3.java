0eb7c20ee497ed3ec913bc48d7d8cc61b614bc8b
Porter <porterrivers444@gmail.com>
2018-04-04 14:41:51 -0400

    Completed Problem 3

--- Problem 3/Problem3.java
+++ Problem 3/Problem3.java
@@ -1,9 +1,16 @@
 import java.lang.Math;
 
 public class Problem3 {
-    private static final long target = 600851475143L; //The value to be factored into primes.
+    private static long target = 600851475143L; //The value to be factored into primes.
 
     public static void main(String[] args){
-
+        int factor = 2;
+        while(target > 1){
+            if(target % factor == 0)
+                target = target / factor; //Remove the prime factor from the target.
+            else
+                factor++;
+        }
+        System.out.println(factor);
     }
 }
